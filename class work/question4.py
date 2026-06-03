72d807a8c9c43cf2aeb062898778beed1b71045d
mantashasheikh <mantashasheikh95844@gmail.com>
2026-06-03 17:58:56 +0530

    python DSA

--- class work/question4.py
+++ class work/question4.py
@@ -1,6 +1,14 @@
 # power of two
-n = int(input("enter a number : "))
-
-for i in range():
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n<=0:
+           return False
+        while n%2==0:
+            n = n//2
+        if n==1:
+            return True
+        else:
+            return False  
+    
        
         
---
+++ class work/question5.py
@@ -0,0 +1,10 @@
+#Running sum
+class Solution(object):
+    def runningSum(self, nums):
+        r = []
+        r.append(nums[0])
+        for i in range(1,len(nums)):
+            a = r[i-1] + nums[i]
+            r.append(a)
+        return r       
+
