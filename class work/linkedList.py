316d30acf20b2d8bb4b1c1c6da9219a06eed8514
mantashasheikh <mantashasheikh95844@gmail.com>
2026-07-03 17:55:23 +0530

    linked list

--- class work/linkedList.py
+++ class work/linkedList.py
@@ -1,22 +1,23 @@
-class node():
-    def __init__(self , data):
-        self.data = data
-        self.next = None
+# class node():
+#     def __init__(self , data):
+#         self.data = data
+#         self.next = None
         
-    def traverse(self,head):
-        c = head
-        while c.next!=None:
-            print(c.data)
-            c = c.next
+#     def traverse(self,head):
+#         c = head
+#         while c.next!=None:
+#             print(c.data)
+#             c = c.next
                 
         
-n1 = node(100)
-n2 = node(200)
-n3 = node(300)
-n1.next = n2
-n2.next = n3
-head = n1
-n1.traverse(head)
+# n1 = node(100)
+# n2 = node(200)
+# n3 = node(300)
+# n1.next = n2
+# n2.next = n3
+
+# head = n1
+# n1.traverse(head)
 
 
 
